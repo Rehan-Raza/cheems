414a15f4476ccd5a7da77106a2de97ef0bf3b313
Rehan Raza <70847285+Rehan-Raza@users.noreply.github.com>
2023-09-19 17:19:03 +0530

    Update login.php

--- login.php
+++ login.php
@@ -85,7 +85,7 @@ body{
 
 
 <div>
-<h2 class="text-white"><center><font size="10">Emp management system</font></center></h2>
+<h2 class="text-white"><center><font size="10">Emp1 management system</font></center></h2>
 
 </div><br>
 
